bccf8e998ec89195cacbdaf1686ffc1ac6e2931c
Bhavik <bhavikcharola@gmail.com>
2017-09-14 10:02:07 +0530

    Vesrion 0.1

---
+++ Assets/Scripts/ObjectMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    int activeWaypointIndex = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = WaypointsManager.instance.waypoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, WaypointsManager.instance.waypoints[activeWaypointIndex].position, .05f);
+        if (transform.position == WaypointsManager.instance.waypoints[activeWaypointIndex].position)
+            if (activeWaypointIndex < WaypointsManager.instance.waypoints.Count - 1)
+                activeWaypointIndex++;
+            else
+                activeWaypointIndex = 0;
+    }
+}
